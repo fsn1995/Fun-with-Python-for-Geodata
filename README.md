1d75b3ef3759283adf1aaa6186c599e264349262
Shunan Feng <fsn.1995@gmail.com>
2020-02-22 21:41:42 +0100

    rasterio use case

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Some tools needed by myself in study. Descriptions will be given in the script.
 1. EarthdataDownload.py
 This is used to bulk download data from earthdata.nasa.gov. Input required is the link list.
 2. mosaic.py
-This is used to do mosaic for geotiff data. Put the pre-processing image at the same folder and run.
+This is used to do mosaic and subset by roi for geotiff data. The process is done with rasterio library.
--- mosaic.py
+++ mosaic.py
@@ -1,12 +1,18 @@
 """
 # =============================================================================
-This script aims to do imagery mosaic automatically. It reads and exports in 
-geotiff format. Note: error may happen if the image size exceeds the memory
-limit.
+This script aims to do simple tasks of imagery mosaic and subset by roi
+automatically. It reads and exports in geotiff format. The exported image
+will be compressed by default to save the space.
+Note: error may happen if the image size exceeds the memory limit.
+Some also report that the exported image cannot be read by ArcGIS desktop.
 
 reference:
 https://rasterio.readthedocs.io/en/latest/quickstart.html
 https://automating-gis-processes.github.io/CSC18/lessons/L6/raster-mosaic.html
+
+The compression may take a while as it will try to optimze the storage.
+For comparison of the compression methods:
+https://kokoalberti.com/articles/geotiff-compression-optimization-guide/
 
 Shunan Feng fsn.1995@gmail.com
 # =============================================================================
@@ -17,17 +23,28 @@ Shunan Feng fsn.1995@gmail.com
 import rasterio
 from rasterio.merge import merge
 from rasterio.plot import show
+import rasterio.mask
 
 import glob
 import os
-
+import fiona
 # define the folder that contains the images
-folderpath = r"C:\Users\A409118\Desktop\IQ_Rufaiat_19Feb19"
+folderpath = r"P:\GIS\GIS\Nigeria\2020\Pix\wathab2020\NG_Gwoza_27Jan20"
 # define the full path for your output mosaic image
-fileout = r"C:\Users\A409118\Desktop\IQ_Rufaiat_19Feb19\IQmosaic.tif"
+mosaicout = r"P:\GIS\GIS\Nigeria\2020\Pix\wathab2020\NG_Gwoza_27Jan20.tif"
 
+# =============================================================================
+# if you would like to clip by roi, change the clipFlag to 1 and specifiy the 
+# shapefile path
+clipFlag = 0
+clipout = r"P:\GIS\GIS\Nigeria\2020\Pix\wathab2020\NG_Ngala_09Jan20clip.tif"
+shapepath = r"P:\GIS\GIS\Nigeria\2020\Pix\wathab2020\NG_Ngala_09Jan20\ngala.shp"
+# =============================================================================
+
+
+    
 # read images in the folder
-searchCriteria = "I*.tif"
+searchCriteria = "*.tif"
 globInput = os.path.join(folderpath, searchCriteria)
 tifList = glob.glob(globInput)
 
@@ -36,14 +53,32 @@ for filepath in tifList:
     src = rasterio.open(filepath)
     tifList2mosaic.append(src)
 immosaic, outTransform = merge(tifList2mosaic)
+
 show(immosaic)
 
 out_meta = src.meta.copy()
 out_meta.update({"driver": "GTiff",
-                         "height": immosaic.shape[1],
-                          "width": immosaic.shape[2],
-                          "transform": outTransform
-                          }
-                         )
-with rasterio.open(fileout, "w", **out_meta) as dst:
+                 "height": immosaic.shape[1],
+                 "width": immosaic.shape[2],
+                 "transform": outTransform
+#                 "compress": "lzw"
+                 })
+
+with rasterio.open(mosaicout, "w", **out_meta, COMPRESS="lzma") as dst:
              dst.write(immosaic)
+
+# read shapefile
+if clipFlag == 1:         
+    with fiona.open(shapepath, "r") as shapefile:
+        shapes = [feature["geometry"] for feature in shapefile]
+    with rasterio.open(mosaicout) as src:
+        img, imgTransform = rasterio.mask.mask(src, shapes, crop=True)
+        imgMeta = src.meta
+    show(img)
+    imgMeta.update({"driver": "GTiff",
+                 "height": img.shape[1],
+                 "width": img.shape[2],
+                 "transform": imgTransform})
+
+    with rasterio.open(clipout, "w", **imgMeta,  COMPRESS="lzma") as dst:
+        dst.write(img)
